91e9543b71fbdcffecf1b49801d77a339e09ea1a
Sethu Mathavan Subramanian <sethutks@gmail.com>
2018-02-12 11:09:51 +0530

    Implicit Wait

--- src/main/java/week2/day1/LearnDropdown.java
+++ src/main/java/week2/day1/LearnDropdown.java
@@ -1,6 +1,7 @@
 package week2.day1;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,19 +9,22 @@ import org.openqa.selenium.support.ui.Select;
 
 public class LearnDropdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException  {
 		// Open the browser
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		
+
 		// Load the URL
 		driver.get("http://leaftaps.com/opentaps/");
 
 		// Maximize the browser
 		driver.manage().window().maximize();
+		
+		//Implicit Wait - It affects only findElement
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
 		// Find the username and enter the value
-		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("username1").sendKeys("DemoSalesManager");
 
 		// Find the password and enter the value
 		driver.findElementById("password").sendKeys("crmsfa");
@@ -36,7 +40,6 @@ public class LearnDropdown {
 
 		// click the link Create Lead
 		driver.findElementByLinkText("Create Lead").click();
-		
 		WebElement source = 
 				driver.findElementById("createLeadForm_dataSourceId");
 		Select dd = new Select(source);
@@ -49,32 +52,44 @@ public class LearnDropdown {
 		for (WebElement eachOpt : opt) {
 			System.out.println(eachOpt.getText());
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		WebElement country = 
+				driver.findElementById("createLeadForm_generalCountryGeoId");
+		Select countryName = new Select(country);
+		countryName.selectByVisibleText("India");
+		Thread.sleep(3000);
+		WebElement state = 
+				driver.findElementById("createLeadForm_generalStateProvinceGeoId");
+		Select stateName = new Select(state);
+		stateName.selectByVisibleText("TAMILNADU");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	}
 
