d1fcfbc53a2e1999a9bf243e78046778891cf93d
Sethu Mathavan Subramanian <sethutks@gmail.com>
2018-02-23 12:49:35 +0530

    Selenium Grid - Code Added

--- src/main/java/week3/day5/LearnActions.java
+++ src/main/java/week3/day5/LearnActions.java
@@ -1,20 +1,28 @@
 package week3.day5;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class LearnActions {
 @Test
-	public void main() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", 
-				"./drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.flipkart.com/");
+	public void main() throws InterruptedException, MalformedURLException {
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("firefox");
+		dc.setPlatform(Platform.WINDOWS);
+		
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://172.24.200.182:4444/wd/hub"), dc);
+		
+	driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		List<WebElement> list = driver.findElementsByXPath("//button[text()='✕']");
